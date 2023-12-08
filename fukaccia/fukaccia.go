5111c66bf6ce143f800795289e435e0e3a0706e7
Ivan Markin <git@unkaktus.art>
2023-12-08 13:51:55 +0100

    Read and write level files

--- fukaccia/fukaccia.go
+++ fukaccia/fukaccia.go
@@ -6,9 +6,17 @@ package main
 import "C"
 
 import (
+	"bufio"
+	"encoding/binary"
+	"errors"
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"os"
 	"runtime"
+	"strconv"
+	"strings"
 	"unsafe"
 )
 
@@ -101,9 +109,112 @@ func InterpolateID(req InterpolationRequest) Fields {
 	return Fields{}
 }
 
+func ParseVarHeader(h string) (string, int, error) {
+	h = strings.TrimRight(h, "\n")
+	sp := strings.Split(h, " : ")
+	varnameKV := strings.Split(sp[0], " = ")
+	if len(varnameKV) != 2 {
+		return "", 0, fmt.Errorf("malformed header")
+	}
+	lengthKV := strings.Split(sp[1], " = ")
+	if len(lengthKV) != 2 {
+		return "", 0, fmt.Errorf("malformed header")
+	}
+	varname := varnameKV[1]
+	length, err := strconv.Atoi(lengthKV[1])
+	if err != nil {
+		return "", 0, fmt.Errorf("convert length to int")
+	}
+	return varname, length, nil
+
+}
+
+func ReadVariable(r *bufio.Reader) (string, []float64, error) {
+	varString, err := r.ReadString('\n')
+	if err != nil {
+		return "", nil, fmt.Errorf("read var string: %w", err)
+	}
+	varname, length, err := ParseVarHeader(varString)
+	if err != nil {
+		return "", nil, fmt.Errorf("parse variable header: %w", err)
+	}
+	v := make([]float64, length)
+	if err := binary.Read(r, binary.LittleEndian, &v); err != nil {
+		return "", nil, fmt.Errorf("read binary: %w", err)
+	}
+	return varname, v, nil
+}
+
+func ReadLevelFile(filename string) (map[string][]float64, error) {
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		return nil, fmt.Errorf("open file: %w", err)
+	}
+	r := bufio.NewReader(file)
+
+	m := map[string][]float64{}
+
+	for {
+		_, err = r.ReadString('\n')
+		if err != nil {
+			return nil, fmt.Errorf("read begin")
+		}
+		varname, value, err := ReadVariable(r)
+		if errors.Unwrap(err) == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		m[varname] = value
+	}
+
+	return m, nil
+}
+
+var headerString = "$BEGIN_variables:\n"
+
+func WriteLevelFile(filename string, m map[string][]float64) error {
+	file, err := os.Create(filename)
+	defer file.Close()
+	if err != nil {
+		return fmt.Errorf("create file: %w", err)
+	}
+	w := bufio.NewWriter(file)
+	defer w.Flush()
+
+	_, err = w.WriteString(headerString)
+	if err != nil {
+		return fmt.Errorf("write header: %w", err)
+	}
+	for varname, values := range m {
+		_, err = fmt.Fprintf(w, "$variable = %s : length = %d\n", varname, len(values))
+		if err != nil {
+			return fmt.Errorf("write header for variable %s: %w", varname, err)
+		}
+		err = binary.Write(w, binary.LittleEndian, values)
+		if err != nil {
+			return fmt.Errorf("write values for variable %s: %w", varname, err)
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	filename := flag.String("f", "", ".info file")
 	flag.Parse()
+
+	m, err := ReadLevelFile("level3.173")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = WriteLevelFile("outlevel", m)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	bi := ReadBinaryInfo(*filename, BHNS)
 	log.Printf("%+v", bi)
